a96cfd570b87d7ce552d00872dde8be0aad044c6
Vishalshelke26 <vishalshelke2699@gmail.com>
2025-12-21 12:27:59 +0530

    "new changes in index css and main file"

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, request, jsonify, render_template
 from src.util import ChurnCustomer
-from src import config
 
 app = Flask(__name__)
 
@@ -12,14 +11,14 @@ def home():
 def churn_cust():
     data = request.form
     obj = ChurnCustomer(data)
-    predicted_churn = obj.predict_charges()
 
-    result = int(predicted_churn)
-    final_output = "Churn" if result == 1 else "Will Repeat Next Time"
+    prediction, probability = obj.predict_charges()
 
     return jsonify({
-        "prediction": final_output
+        "prediction": "Churn" if int(prediction) == 1 else "Will Repeat Next Time",
+        "probability": round(probability * 100, 2)
     })
 
 if __name__ == "__main__":
-    app.run(debug=True, port=config.FLASK_PORT_NUMBER)
+    app.run(debug=True)
+
